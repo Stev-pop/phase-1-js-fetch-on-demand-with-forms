ca88f3dd9c660a8c6123968bb9cc0b83aea493e2
Stephen Onyango <onyangosteve01@gmail.com>
2024-04-05 22:37:53 +0300

    Adds full assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+    document.addEventListener("DOMContentLoaded", ()=>{
+
+    const form = document.querySelector("form");
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault()
+        const codeInserted = document.querySelector('input#searchByID')
+        console.log(codeInserted.value)
+        
+        fetch(`http://localhost:3000/movies/${codeInserted.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+
+
+            const title = document.querySelector("#form-heading")
+            const summary = document.querySelector("#form-summary")
+            
+            title.innerText = data.title
+            summary.innerText = data.summary
+            
+
+          // LOG: (3) [{…}, {…}, {…}]
+        });
+    })
+})
